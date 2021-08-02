abdff763ff4adeb874c1f1ae49e1a112c9e53ff4
RinaHan <rinahan4146@gmail.com>
2021-08-02 15:53:50 -0700

    working on text components

--- src/App.js
+++ src/App.js
@@ -2,57 +2,49 @@ import React, { useState, useEffect } from "react";
 import App from "./App.scss";
 import Form from "./comps/Input";
 import styled from "styled-components";
-import ScrollToBottom from 'react-scroll-to-bottom';
-
-const Select = styled.select`
-  width: 170px;
-  padding: 10px;
-  border: none;
-  border-radius: 5px;
-  font-weight: bold;
-  color: #6565cd;
-`;
+import ScrollToBottom from "react-scroll-to-bottom";
 
 export default function Allownace() {
-  const [fred, setFred] = useState([])
-  const [yujun, setYujun] = useState([])
+  const [fred, setFred] = useState([]);
+  const [yujun, setYujun] = useState([]);
 
   const data = [
-    {name:'fred', date:'2020-20-20', money:'$20'},
-    {name:'fred', date:'2020-20-20', money:'$22'},
-    {name:'fred', date:'2020-20-20', money:'$23'},
-    {name:'fred', date:'2020-20-20', money:'$20'},
-    {name:'fred', date:'2020-20-20', money:'$22'},
-    {name:'fred', date:'2020-20-20', money:'$23'},
-    {name:'fred', date:'2020-20-20', money:'$20'},
-    {name:'fred', date:'2021-20-20', money:'$22'},
-    {name:'fred', date:'2021-20-20', money:'$23'},
-    {name:'fred', date:'2021-20-20', money:'$20'},
-    {name:'yujun', date:'2020-20-20', money:'$20'},
-    {name:'yujun', date:'2020-20-20', money:'$30'},
-    {name:'yujun', date:'2020-20-20', money:'$40'},
-    {name:'yujun', date:'2020-20-20', money:'$20'},
-    {name:'yujun', date:'2020-20-20', money:'$30'},
-    {name:'yujun', date:'2020-20-20', money:'$40'},
-    {name:'yujun', date:'2021-20-20', money:'$20'},
-    {name:'yujun', date:'2021-20-20', money:'$30'},
-    {name:'yujun', date:'2021-20-20', money:'$40'},
-  ]
-   
+    { name: "fred", date: "2020-20-20", money: "$20" },
+    { name: "fred", date: "2020-20-20", money: "$23" },
+    { name: "fred", date: "2020-20-20", money: "$2" },
+    { name: "fred", date: "2020-20-20", money: "$223" },
+    // { name: "fred", date: "2020-20-20", money: "$20" },
+    // { name: "fred", date: "2020-20-20", money: "$22" },
+    // { name: "fred", date: "2020-20-20", money: "$23" },
+    // { name: "fred", date: "2020-20-20", money: "$20" },
+    // { name: "fred", date: "2021-20-20", money: "$22" },
+    // { name: "fred", date: "2021-20-20", money: "$23" },
+    // { name: "fred", date: "2021-20-20", money: "$20" },
+    { name: "yujun", date: "2020-20-20", money: "$20" },
+    { name: "yujun", date: "2020-20-20", money: "$30" },
+    { name: "yujun", date: "2020-20-20", money: "$40" },
+    { name: "yujun", date: "2020-20-20", money: "$20" },
+    { name: "yujun", date: "2020-20-20", money: "$30" },
+    { name: "yujun", date: "2020-20-20", money: "$40" },
+    { name: "yujun", date: "2021-20-20", money: "$20" },
+    { name: "yujun", date: "2021-20-20", money: "$30" },
+    { name: "yujun", date: "2021-20-20", money: "$40" },
+  ];
+
   //질문: filter는 true인값을 array로 생성된다고 했는데, const fred는 [] 없음 error 남
   const filterData = () => {
-    var fredArr = data.filter(o=> o.name === 'fred')
-    setFred(fredArr)
+    var fredArr = data.filter((o) => o.name === "fred");
+    setFred(fredArr);
 
-    var yujunArr = data.filter(o=> o.name === 'yujun')
-    setYujun(yujunArr)
-  }
+    var yujunArr = data.filter((o) => o.name === "yujun");
+    setYujun(yujunArr);
+  };
 
-  useEffect(()=>{
-  filterData()
-  },[])
+  useEffect(() => {
+    filterData();
+  }, []);
 
-  // console.log(fredArr)
+  // console.log(selectMap)
   return (
     <div className="App">
       <div className="Main">
@@ -61,10 +53,24 @@ export default function Allownace() {
           <div className="Section">
             <div className="NameCont">Fred</div>
             <ScrollToBottom className="NoteCont">
-              {fred.map((o,i)=>{
-                return(
-                  <div className="TextCont">{o.date}, <b>{o.money}</b></div>
-                )
+              {fred.map((o, i) => {
+                return (
+                  <div className="TextCont">
+                    <div className="DateText">
+                      {o.date}, 
+                    </div>
+                    <div className="MoneyText">
+                    {o.money}
+                    </div>
+                    <div className="Xbtn">
+                      <button>x</button>
+                    </div>
+                    <div className="Vbtn">
+                      <button>x</button>
+                    </div>
+                    {/* {on==='ac' ? <button>E</button> : null} */}
+                  </div>
+                );
               })}
             </ScrollToBottom>
           </div>
@@ -72,10 +78,12 @@ export default function Allownace() {
           <div className="Section">
             <div className="NameCont">Yujun</div>
             <ScrollToBottom className="NoteCont">
-              {yujun.map((o,i)=>{
-                return(
-                  <div className="TextCont">{o.date}, <b>{o.money}</b></div>
-                )
+              {yujun.map((o, i) => {
+                return (
+                  <div className="TextCont">
+                    {o.date}, <b>{o.money}</b>
+                  </div>
+                );
               })}
             </ScrollToBottom>
           </div>
@@ -86,10 +94,10 @@ export default function Allownace() {
             <label className="Label" for="select">
               Select Name
             </label>
-            <Select className="Select" id="select">
+            <select className="Select" id="select">
               <option label="Fred" value="fred" />
               <option label="Yujun" value="yujun" />
-            </Select>
+            </select>
           </div>
           <div className="Inputs">
             <label className="Label" for="date">
@@ -98,7 +106,9 @@ export default function Allownace() {
             <Form type="date" />
           </div>
           <div className="Inputs">
-            <label className="Label" for="money">Add allowance</label>
+            <label className="Label" for="money">
+              Add allowance
+            </label>
             <Form type="number" placeholder="$" />
           </div>
           <button className="Btn">Add</button>
